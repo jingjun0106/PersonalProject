4e8e5ca008426285901359cd258e6cbab56e06de
jingjun <jingjun0106@opentask.co.kr>
2018-11-06 12:12:49 +0900

    add pod file

--- PersonalProject/ViewController.swift
+++ PersonalProject/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Alamofire.request("baidu.com")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
